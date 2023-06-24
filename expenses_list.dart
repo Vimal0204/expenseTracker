52c5bcf1808304f844a83876ad71a76279fdf758
Vimal0204 <80668288+Vimal0204@users.noreply.github.com>
2023-06-24 16:21:11 +0530

    Add files via upload

---
+++ expenses_list.dart
@@ -0,0 +1,32 @@
+import 'package:expense_tracker/model/expense.dart';
+import 'package:expense_tracker/widget/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
+  final void Function(Expense expense) onRemoveExpense;
+  final List<Expense> expenses;
+  @override
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.5),
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
+        child: ExpenseItem(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+      ),
+    );
+  }
+}
